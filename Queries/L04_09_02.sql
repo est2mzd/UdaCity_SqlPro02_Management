ce93e37d2b3f467e5944addaa4271b156ef211db
est2mzd <est2mzd@gmail.com>
2020-10-31 23:35:51 +0900

    update modifying files

--- Queries/L04_09_02.sql
+++ Queries/L04_09_02.sql
@@ -1,3 +1,24 @@
 UPDATE people SET last_name =
     SUBSTR(last_name, 1,1) ||
     LOWER(SUBSTR(last_name,2));
+
+
+-- Change born_ago to data_of birth
+
+<1> error
+SELECT CURRENT_TIMESTAMP - born_ago FROM people;
+
+<2>
+SELECT CURRENT_TIMESTAMP - born_ago::INTERVAL FROM people;
+
+<3>
+SELECT (CURRENT_TIMESTAMP - born_ago::INTERVAL)::DATE FROM people;
+
+-----------
+
+ALTER TABLE people ADD COLUMN date_of_birth DATE;
+
+UPDATE people SET date_of_birth = 
+    (CURRENT_TIMESTAMP - born_ago::INTERVAL)::DATE;
+
+ALTER TABLE people DROP COLUMN born_ago;
