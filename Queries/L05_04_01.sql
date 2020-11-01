ff69a4895bc66f4005db48908c746e3c642ffdd1
est2mzd <est2mzd@gmail.com>
2020-11-01 15:28:48 +0900

    update modifying files

--- Queries/L05_04_01.sql
+++ Queries/L05_04_01.sql
@@ -9,10 +9,18 @@ INSERT INTO users (id, username) VALUES
     (1, 'user1'),
     (1, 'user2');
 
+SELECT * FROM users;
+
+
 /* Add Constraint */
+TRUNCATE TABLE users;
 ALTER TABLE users ADD UNIQUE (id);
+
 
 /* Set Data */
 INSERT INTO users (username) VALUES
     (NULL),
     (NULL);
+
+SELECT * FROM users;    
+
