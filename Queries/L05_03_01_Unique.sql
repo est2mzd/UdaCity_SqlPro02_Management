8207de0d05199d3c7bc200d55ff8ba6c6441879d
est2mzd <est2mzd@gmail.com>
2020-11-01 14:25:07 +0900

    update modifying files

--- Queries/L05_03_01_Unique.sql
+++ Queries/L05_03_01_Unique.sql
@@ -5,11 +5,19 @@ CREATE TABLE users(
     "username" VARCHAR
 );
 
-ALTER TABLE users ADD UNIQUE (user_name);
+ALTER TABLE users ADD UNIQUE (username);
 
 
 /* Create Table -> add Unique property <2> */
-CREATE TABLE users(
+CREATE TABLE users_2(
     "id" SERIAL,
     "username" VARCHAR UNIQUE
 );
+
+
+/* Set Data <1> */
+INSERT INTO users (username) VALUES ('user1');
+INSERT INTO users (username) VALUES ('user2');
+
+/* Set Data <2> ==> Error */
+UPDATE users SET username='user1' WHERE id = 2;
