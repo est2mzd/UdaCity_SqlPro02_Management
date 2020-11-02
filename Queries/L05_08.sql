c2fbdc05cc43789695c7ede5859823fadfaf5b02
est2mzd <est2mzd@gmail.com>
2020-11-02 22:45:03 +0900

    update modifying files

--- Queries/L05_08.sql
+++ Queries/L05_08.sql
@@ -33,3 +33,25 @@ DELETE FROM users_2
 
 ERROR:  update or delete on table "users_2" violates foreign key constraint "comments_2_user_id_fkey" on table "comments_2"
 DETAIL:  Key (id)=(1) is still referenced from table "comments_2".
+
+
+/* ------------- Retry ---------------- */
+/* Preparation */
+DROP TABLE comments_2;
+
+/* Create Table 3 */
+CREATE TABLE comments_2(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users_2 (id) ON DELETE CASCADE,
+    content TEXT
+);
+
+/* Set Data 3 */
+INSERT INTO comments_2 (user_id, content) VALUES
+    (1, 'hello world'),
+    (2, 'lorem ipsum'),
+    (3, 'another comment');
+
+/* DELETE Rows 2 */
+DELETE FROM users_2
+    WHERE id = 1;    
