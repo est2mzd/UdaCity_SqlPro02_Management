1092bd6d307450b10f0725f796518d7a7bfe5f9d
est2mzd <est2mzd@gmail.com>
2020-11-09 16:31:39 +0900

    mod @ u

--- Queries/L07_08.js
+++ Queries/L07_08.js
@@ -24,11 +24,30 @@ db.events.find({}).pretty().limit(2)
 
 // Q1: The total number of events in the collection
 db.events.countDocuments({})
+    -->225000
 
 // Q2: The total number of events for the device with ID 8f5844d2-7ab3-478e-8ea7-4ea05ab9052e
-db.events.countDocuments({_id: '8f5844d2-7ab3-478e-8ea7-4ea05ab9052e'})
+db.events.countDocuments({deviceId: '8f5844d2-7ab3-478e-8ea7-4ea05ab9052e'})
+    --> 240
 
 // Q3: The total number of events that came from a Firefox browser and happened on or after April 20th, 2019
-
+db.events.countDocuments(
+    {'browser.vendor': 'firefox'},
+    {timestamp: {$gte: ISODate('2019-04-20')}}
+)
+    --> 67215
 
 // Q4: The list of the top 100 events that happened in Chrome on Windows, sorted in reverse chronological order
+db.events.find({
+    'browser.vendor': 'chrome',
+    'browser.os': 'windows',
+}).sort({
+    timestamp: -1
+}).limit(100)
+
+// alternative solution for Q4
+db.events.find({
+    browser:{vendor: 'chrome', os:'windows'}
+}).sort({
+    timestamp: -1
+}).limit(100)
