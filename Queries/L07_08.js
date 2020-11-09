1ca86923c3beaa0f1eded6228d9038c70c588642
est2mzd <est2mzd@gmail.com>
2020-11-09 16:14:22 +0900

    mod

--- Queries/L07_08.js
+++ Queries/L07_08.js
@@ -19,3 +19,16 @@ show collections
 
 // Check Data
 db.events.find({}).pretty().limit(2)
+
+//-------------------------------------------------------------------------------------//
+
+// Q1: The total number of events in the collection
+db.events.countDocuments({})
+
+// Q2: The total number of events for the device with ID 8f5844d2-7ab3-478e-8ea7-4ea05ab9052e
+db.events.countDocuments({_id: '8f5844d2-7ab3-478e-8ea7-4ea05ab9052e'})
+
+// Q3: The total number of events that came from a Firefox browser and happened on or after April 20th, 2019
+
+
+// Q4: The list of the top 100 events that happened in Chrome on Windows, sorted in reverse chronological order
