8d352d8f31f3d3bef9801155c42c85dae5b0d475
est2mzd <est2mzd@gmail.com>
2020-11-10 00:10:53 +0900

    mod

--- Queries/L07_14.js
+++ Queries/L07_14.js
@@ -1,21 +1,33 @@
 // Listの取り扱い
 
 LPUSH numbers 1
-  -->
+  --> (integer) 1
 
 LPUSH numbers 2
--->
+  --> (integer) 2
 
 LPUSH numbers 3
--->
+  --> (integer) 3
+
+LPUSH numbers 4 5 6
+  --> (integer) 6
+
 
 LLEN numbers
--->
+  --> (integer) 6
+
 
 LRANGE numbers 0 2
--->
+  --> 1) "6"
+      2) "5"
+      3) "4"
 
 LRANGE numbers 0 -1
--->
+--> 1) "6"
+    2) "5"
+    3) "4"
+    4) "3"
+    5) "2"
+    6) "1"
 
 
